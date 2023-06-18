2d1ed9ac2232321579d726aa63581831583130aa
Rajat Patel <rpatel@interos.ai>
2023-06-18 04:56:36 +0000

    add preprocess and train file datasets

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -16,8 +16,8 @@ from sklearn.preprocessing import LabelEncoder
 
 
 class ContrastivePretrainDataset(torch.utils.data.Dataset):
-    def __init__(self, path, deduction_set, tokenizer="roberta-base", max_length=128, 
-                intermediate_set=None, clean=False, dataset="causal_news", only_interim=False, 
+    def __init__(self, path, tokenizer="roberta-base", max_length=128, 
+                dataset='causal-news',
                 aug=False) -> None:
         super().__init__()
 
@@ -25,3 +25,45 @@ class ContrastivePretrainDataset(torch.utils.data.Dataset):
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer, additional_special_tokens=[])
         self.dataset = dataset
         self.aug = aug
+        data = pd.read_csv(path)
+        cluster_id_set = [x for x in range(len(data))]
+        data1 = data.copy()
+        data1['cluster_id'] = cluster_id_set
+        data2 = data.copy()
+        data2['cluster_id'] = cluster_id_set
+        data2 = data.copy()
+        label_enc = LabelEncoder()
+        label_enc.fit(cluster_id_set)
+        data1['features'] = data1['text']
+        data2['features'] = data2['text']
+        data1['labels'] = label_enc.transform(data1['cluster_id'])
+        data2['labels'] = label_enc.transform(data2['cluster_id'])
+        self.label_encoder = label_enc
+
+        data1 = data1.reset_index(drop=True)
+        data1 = data1.fillna("")
+        data1 = self._prepare_data(data1)
+        self.data1 = data1
+        self.data2 = data2
+
+
+    def __getitem__(self, idx):
+        example1 = self.data1.loc[idx].copy()
+        selection1 = self.data1[self.data1['labels'] == example1['labels']]
+        pos1 = selection1.sample(1).iloc[0].copy()
+
+        example2 = self.data2.loc[idx].copy()
+        selection2 = self.data2[self.data2['labels'] == example2['labels']]
+        pos2 = selection2.sample(1).iloc[0].copy()
+
+        return ((example1, pos1), (example2, pos2))
+
+    def __len__(self):
+        return len(self.data1)
+
+    
+
+
+
+
+        
--- train.py
+++ train.py
@@ -1,4 +1,57 @@
 import transformers
 import torch
 from src.model import ContrastivePretrainModel
-from torhc
+import torch
+import pandas as pd
+from src.data.datasets import ContrastivePretrainDataset
+from transformers import EarlyStoppingCallback
+from transformers.trainer_utils import get_last_checkpoint
+from transformers.file_utils import is_offline_mode
+from transformers.utils import check_min_version
+from transformers.utils.versions import require_version
+from transformers import (
+    HfArgumentParser, 
+    Trainer,
+    TrainingArguments,
+    set_seed
+)
+from dataclasses import dataclass, field
+from typing import Optional
+
+
+@dataclass
+class ModelArguments:
+    model_pretrained_checkpoint:Optional[str] = field()
+    do_param_opt:Optional[bool] = field()
+    grad_checkpoint:Optional[bool] = field()
+    temperature:Optional[float] = field()
+    tokenizer:Optional[str] = field()
+
+
+@dataclass
+class DataTrainingArguments:
+    train_file:Optional[str] = field()
+    valid_file:Optional[str] = field()
+    augment:Optional[str] = field()
+    train_size:Optional[str] = field()
+    max_train_samples:Optional[str] = field()
+    max_valid_samples:Optional[str] = field()
+
+
+
+
+if __name__ == '__main__':
+    train_path = "/home/jovyan/work/causal-events/data/subtask1/train_subtask1.csv"
+    valid_path = "/home/jovyan/work/causal-events/data/subtask1/dev_subtask1.csv"
+
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+
+    train_data = ContrastivePretrainDataset(path=train_path, tokenizer='roberta-base', max_length=128)
+    valid_data = ContrastivePretrainDataset(path=valid_path, tokenizer='roberta-base', max_length=128)
+    model = ContrastivePretrainModel(len_tokenizer=128, model='roberta-base', pool='True', proj='mlp', temperature=0.07)
+
+
+
+
+
+
